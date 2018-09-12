61fd15f1e92ff080a23467ee8164a5a119c9e00e
jason-linthwaite <jason@sciapps.co.uk>
2018-09-12 10:52:21 +0100

    Add Perl

--- scripts/sciapps-generate.sh
+++ scripts/sciapps-generate.sh
@@ -2,7 +2,7 @@
 
 echo "FROM docker:17.05.0-ce-git"
 
-echo "RUN apk --update add --no-cache py-pip=9.0.0-r1 unzip bash"
+echo "RUN apk --update add --no-cache py-pip=9.0.0-r1 unzip bash perl"
 
 echo "RUN pip install docker-compose==1.12.0 awscli==1.11.76"
 
