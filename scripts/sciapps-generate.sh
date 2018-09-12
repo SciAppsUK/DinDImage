6d8932a4ae74f88572a9ecea212d62986eae29d0
jason-linthwaite <jason@sciapps.co.uk>
2018-09-12 17:38:19 +0100

    Fix curl

--- scripts/sciapps-generate.sh
+++ scripts/sciapps-generate.sh
@@ -6,5 +6,5 @@ echo "RUN apk --update add --no-cache py-pip=9.0.0-r1 unzip bash perl curl curl-
 
 echo "RUN pip install docker-compose==1.12.0 awscli==1.11.76"
 
-echo "curl -L -o /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5rc1/jq-linux-x86_64-static \
+echo "RUN curl -L -o /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5rc1/jq-linux-x86_64-static \
                                 && chmod +x /usr/bin/jq"
