116613c6fd8f32cda399407a6a5f3b6cd0fb789e
jason-linthwaite <jason@sciapps.co.uk>
2018-09-12 10:45:44 +0100

    Try building custom dind image start

---
+++ scripts/sciapps-generate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "FROM docker:17.05.0-ce-git"
+
+echo "RUN apk --update add --no-cache py-pip=9.0.0-r1 unzip "
+
+echo "RUN pip install docker-compose==1.12.0 awscli==1.11.76"
+
+# install bats for testing
+echo "RUN git clone https://github.com/sstephenson/bats.git \
+  && cd bats \
+  && ./install.sh /usr/local \
+  && cd .. \
+  && rm -rf bats"
+
+# install dependencies for tap-to-junit
+echo "RUN perl -MCPAN -e 'install TAP::Parser'"
+echo "RUN perl -MCPAN -e 'install XML::Generator'"
