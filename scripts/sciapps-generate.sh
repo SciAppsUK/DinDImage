fb3e755578e4aef15270cafebcf85f2ae793fbbe
jason-linthwaite <jason@sciapps.co.uk>
2018-09-13 10:26:28 +0100

    Update docker engine and versions

--- scripts/sciapps-generate.sh
+++ scripts/sciapps-generate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "FROM docker:17.05.0-ce-git"
+echo "FROM docker:18.06.1-ce-git"
 
 echo "RUN apk --update add --no-cache py-pip=9.0.0-r1 unzip bash perl curl curl-dev jq"
 
-echo "RUN pip install docker-compose==1.12.0 awscli==1.11.76"
+echo "RUN pip install docker-compose==1.22.0 awscli==1.16.12"
 
 echo "RUN curl -L -o /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5rc1/jq-linux-x86_64-static \
                                 && chmod +x /usr/bin/jq"
