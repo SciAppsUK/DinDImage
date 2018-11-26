d3f8122e461de368b8d1e2885a307b3b660e5b81
jason-linthwaite <jason@sciapps.co.uk>
2018-11-26 16:59:47 +0000

    Update version for aws cli

--- scripts/sciapps-generate.sh
+++ scripts/sciapps-generate.sh
@@ -5,7 +5,7 @@ echo "FROM docker:18.06.1-ce-git"
 
 echo "RUN apk --update add --no-cache py2-pip unzip bash perl curl curl-dev jq"
 
-echo "RUN pip install docker-compose==1.22.0 awscli==1.16.12"
+echo "RUN pip install docker-compose==1.22.0 awscli"
 
 echo "RUN curl -L -o /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5rc1/jq-linux-x86_64-static \
                                 && chmod +x /usr/bin/jq"
