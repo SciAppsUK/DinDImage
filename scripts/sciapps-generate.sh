d75b20c68edf324cac403077345eca1c3c2fa3cf
jason-linthwaite <jason@sciapps.co.uk>
2019-01-28 20:39:59 +0000

    Update docker compose config

--- scripts/sciapps-generate.sh
+++ scripts/sciapps-generate.sh
@@ -5,7 +5,7 @@ echo "FROM docker:18.06.1-ce-git"
 
 echo "RUN apk --update add --no-cache py2-pip unzip bash perl curl curl-dev jq"
 
-echo "RUN pip install docker-compose==1.22.0 awscli"
+echo "RUN pip install awscli"
 
 echo "RUN curl -L -o /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5rc1/jq-linux-x86_64-static \
                                 && chmod +x /usr/bin/jq"
